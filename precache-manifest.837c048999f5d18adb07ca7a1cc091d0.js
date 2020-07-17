31151ba2ba0f115af43541aff4365ca3142b4d9d
Daniel <daniel.ioanitescu@gmail.com>
2020-07-17 16:45:02 +0300

    Updates

---
+++ precache-manifest.837c048999f5d18adb07ca7a1cc091d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa33a985941fa66a5e218b5173e27d3e",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "c41731abb73bad86bba2",
+    "url": "/covid/static/css/main.5111a411.chunk.css"
+  },
+  {
+    "revision": "fe445d6b7f147336e0aa",
+    "url": "/covid/static/js/2.9bb1eaba.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covid/static/js/2.9bb1eaba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c41731abb73bad86bba2",
+    "url": "/covid/static/js/main.6e40f2cb.chunk.js"
+  },
+  {
+    "revision": "2538616d296fec9cb619",
+    "url": "/covid/static/js/runtime-main.6f8ac736.js"
+  },
+  {
+    "revision": "c524e3253c7729070dbb7a4b40293d33",
+    "url": "/covid/static/media/logo.c524e325.png"
+  }
+]);
